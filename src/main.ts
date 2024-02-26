8ac205a1f052d6958b182dc47e813036dc03bd68
Balagopal G <balagopal.g@regovtech.com>
2024-02-26 18:14:35 +0800

    WIP : graphql configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app.module';
 import { logger } from 'utils/winston';
+import { AppModule } from './modules/app.module';
 
 async function bootstrap() {
   logger.info('Starting the server');
   const app = await NestFactory.create(AppModule);
+
+  // Add headers here
+  app.use((req, res, next) => {
+    res.setHeader('content-type', 'application/json');
+    next();
+  });
+
   await app.listen(3000, '0.0.0.0', () => {
     logger.info('GraphQL Server started on: http://localhost:3000/graphql');
   });
